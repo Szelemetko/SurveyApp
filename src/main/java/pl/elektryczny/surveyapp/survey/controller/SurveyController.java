3656d78f6ddabf39b63f0623968c99490351fda7
Andrzej Szelemetko <andrzej.szelemetko@projectdrgn.com>
2020-06-04 09:34:07 +0200

    add cross origin

--- src/main/java/pl/elektryczny/surveyapp/survey/controller/SurveyController.java
+++ src/main/java/pl/elektryczny/surveyapp/survey/controller/SurveyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path= {"/survey", "/surveys"})
 public class SurveyController {
 
